3baddd03e9bdaa5860ca015debb7b55c31f1d05f
vladimirtischler <tischler.vladimir@gmail.com>
2019-11-17 09:20:07 +0100

    sdv

---
+++ Open-Lab-04.03/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Open_Lab_04._03
+{
+    class Program
+    {
+        public static string RemoveFirstLast(string original)
+        {
+
+            if (original.Length > 1)
+            {
+                return original.Substring(1, original.Length - 2);
+            }
+            else if (original.Length == 1)
+            {
+                return original;
+            }
+            return original;
+
+        }   
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            Console.WriteLine(RemoveFirstLast(word));
+        }
+    }
+}
